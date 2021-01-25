6973c78aeca6d030a6688f70752fde4ba42302e4
Pranav Fulkari <zspprotonzz@gmail.com>
2021-01-25 20:12:25 +0530

    First Deploy

---
+++ 10_Days_With_AngularJS/js/app.js
@@ -0,0 +1,26 @@
+angular.module('app', [])
+    .controller('appController', function($scope, $locale) {
+
+        $scope.firstName = "Pranav";
+        $scope.lastName = "Fulkari";
+        $scope.title = "Internet Entrepreneur & Realtime Programmer";
+        $scope.email = "director@bottlecaptech.com";
+        $scope.number = "+91-0000000000";
+        $scope.website = "https://bottlecaptech.com";
+        $scope.instagram = "https://www.instagram.com/pprotonzz/";
+        $scope.twitter = "https://www.twitter.com/pprotonzz";
+        $scope.facebook = "https://www.facebook.com/pprotonzz";
+        $scope.youtube = "http://youtube.com/programmingwithpranavfulkari";
+        $scope.linkedin = "https://www.linkedin.com/in/pprotonzz/";
+        $scope.address = "Headquarters : Chinchpokli, L.A.";
+
+        $scope.description = [
+            { text: 'Director of BottleCap Technologies PVT. LTD' },
+            { text: 'Professional Multi-Platform Developer based in Dubai, UAE.' },
+            { text: '👨🏻‍🏫 IEEE Certified Speaker' },
+            { text: '🥇 2 Time Google Awarded Programmer' },
+            { text: '💻 Digital Marketer' },
+            { text: 'Sometimes works as a freelancer.' }
+        ];
+
+    });
